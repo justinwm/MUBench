78d025f1b3b3f5e0bb0d2782dcee49ec0dcb1d3d
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-09-09 18:42:53 +0200

    Remove obsolete parameter.

--- mubench.reviewsite/src/Controller/ExperimentsController.php
+++ mubench.reviewsite/src/Controller/ExperimentsController.php
@@ -7,7 +7,7 @@ use Slim\Http\Response;
 
 class ExperimentsController extends Controller
 {
-    public function index(Request $request, Response $response, array $args)
+    public function index(Request $request, Response $response)
     {
         return $this->renderer->render($response, 'index.phtml');
     }
