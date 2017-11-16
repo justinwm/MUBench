08c082a13a3b24dc6513e681159e057e4297fd06
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-11-16 16:40:17 +0100

    Cleanup routes.

--- mubench.reviewsite/src/routes.php
+++ mubench.reviewsite/src/routes.php
@@ -14,44 +14,53 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 $app->get('/', \MuBench\ReviewSite\Controller\ExperimentsController::class.":index")->setName('/');
-$app->get('/experiments/{experiment_id}/detectors/{detector_muid}/runs', \MuBench\ReviewSite\Controller\RunsController::class.":getIndex")->setName('experiment.detector');
-$app->get('/experiments/{experiment_id}/detectors/{detector_muid}/runs.csv', \MuBench\ReviewSite\Controller\RunsController::class.":downloadRuns")->setName('download.runs');
-$app->get('/experiments/{experiment_id}/detectors/{detector_muid}/projects/{project_muid}/versions/{version_muid}/misuses/{misuse_muid}', \MuBench\ReviewSite\Controller\ReviewController::class.":getReview")->setName('view');
-$app->get('/experiments/{experiment_id}/detectors/{detector_muid}/projects/{project_muid}/versions/{version_muid}/misuses/{misuse_muid}/reviewers/{reviewer_id}', \MuBench\ReviewSite\Controller\ReviewController::class.":getReview")->setName('review');
+$app->group('/experiments/{experiment_id}', function() use ($app) {
+    $app->group('/detectors/{detector_muid}', function () use ($app) {
+        $app->get('/runs', \MuBench\ReviewSite\Controller\RunsController::class . ":getIndex")->setName('experiment.detector');
+        $app->get('/runs.csv', \MuBench\ReviewSite\Controller\RunsController::class . ":downloadRuns")->setName('download.runs');
+        $app->group('/projects/{project_muid}/versions/{version_muid}/misuses/{misuse_muid}', function() use ($app) {
+            $app->get('', \MuBench\ReviewSite\Controller\ReviewController::class . ":getReview")->setName('view');
+            $app->get('/reviewers/{reviewer_id}', \MuBench\ReviewSite\Controller\ReviewController::class . ":getReview")->setName('review');
+        });
+    });
+    $app->get('/results.csv', \MuBench\ReviewSite\Controller\RunsController::class . ":downloadResults")->setName('stats.results.csv');
+});
 $app->get('/results', \MuBench\ReviewSite\Controller\RunsController::class.":getResults")->setName('stats.results');
-$app->get('/experiments/{experiment_id}/results.csv', \MuBench\ReviewSite\Controller\RunsController::class.":downloadResults")->setName('stats.results.csv');
 $app->get('/tags', \MuBench\ReviewSite\Controller\TagController::class.":getTags")->setName('stats.tags');
 $app->get('/types', \MuBench\ReviewSite\Controller\TypeController::class.":getTypes")->setName('stats.types');
 
 
 $app->group('/private', function () use ($app) {
     $app->get('/', \MuBench\ReviewSite\Controller\ExperimentsController::class.":index")->setName('private./');
-    $app->get('/experiments/{experiment_id}/detectors/{detector_muid}/runs', \MuBench\ReviewSite\Controller\RunsController::class.":getIndex")->setName('private.experiment.detector');
-    $app->get('/experiments/{experiment_id}/detectors/{detector_muid}/runs.csv', \MuBench\ReviewSite\Controller\RunsController::class.":downloadRuns")->setName('private.download.runs');
-    $app->get('/experiments/{experiment_id}/detectors/{detector_muid}/projects/{project_muid}/versions/{version_muid}/misuses/{misuse_muid}', \MuBench\ReviewSite\Controller\ReviewController::class.":getReview")->setName('private.view');
-    $app->get('/experiments/{experiment_id}/detectors/{detector_muid}/projects/{project_muid}/versions/{version_muid}/misuses/{misuse_muid}/reviewers/{reviewer_id}', \MuBench\ReviewSite\Controller\ReviewController::class.":getReview")->setName('private.review');
-    $app->get('/experiments/{experiment_id}/reviews/{reviewer_id}/open', \MuBench\ReviewSite\Controller\ReviewController::class.":getTodo")->setName('private.todo');
-    $app->get('/experiments/{experiment_id}/reviews/{reviewer_id}/closed', \MuBench\ReviewSite\Controller\ReviewController::class.":getOverview")->setName('private.overview');
+    $app->group('/experiments/{experiment_id}', function() use ($app) {
+        $app->group('/detectors/{detector_muid}', function() use ($app) {
+            $app->get('/runs', \MuBench\ReviewSite\Controller\RunsController::class . ":getIndex")->setName('private.experiment.detector');
+            $app->get('/runs.csv', \MuBench\ReviewSite\Controller\RunsController::class . ":downloadRuns")->setName('private.download.runs');
+            $app->group('/projects/{project_muid}/versions/{version_muid}/misuses/{misuse_muid}', function() use ($app) {
+                $app->get('', \MuBench\ReviewSite\Controller\ReviewController::class . ":getReview")->setName('private.view');
+                $app->get('/reviewers/{reviewer_id}', \MuBench\ReviewSite\Controller\ReviewController::class . ":getReview")->setName('private.review');
+            });
+        });
+        $app->get('/reviews/{reviewer_id}/open', \MuBench\ReviewSite\Controller\ReviewController::class . ":getTodo")->setName('private.todo');
+        $app->get('/reviews/{reviewer_id}/closed', \MuBench\ReviewSite\Controller\ReviewController::class . ":getOverview")->setName('private.overview');
+        $app->get('/results.csv', \MuBench\ReviewSite\Controller\RunsController::class.":downloadResults")->setName('private.stats.results.csv');
+    });
     $app->get('/results', \MuBench\ReviewSite\Controller\RunsController::class.":getResults")->setName('private.stats.results');
-    $app->get('/experiments/{experiment_id}/results.csv', \MuBench\ReviewSite\Controller\RunsController::class.":downloadResults")->setName('private.stats.results.csv');
     $app->get('/tags', \MuBench\ReviewSite\Controller\TagController::class.":getTags")->setName('private.stats.tags');
     $app->get('/types', \MuBench\ReviewSite\Controller\TypeController::class.":getTypes")->setName('private.stats.types');
 })->add(new \MuBench\ReviewSite\Middleware\AuthMiddleware($container));
 
 $app->group('', function () use ($app, $settings) {
     $app->post('/metadata', \MuBench\ReviewSite\Controller\MetadataController::class.":putMetadata");
-    // /experiments/{experiment_id}/detectors/{detector_muid}/projects/{project_muid}/versions/{version_muid}/misuses/{misuse_muid}/tags/{tag_id}
-
-    $app->post('/experiments/{experiment_id}/detectors/{detector_muid}/projects/{project_muid}/versions/{version_muid}/runs', \MuBench\ReviewSite\Controller\RunsController::class.":postRun");
-
-    $app->group('/experiments/{experiment_id}/detectors/{detector_muid}/projects/{project_muid}/versions/{version_muid}/misuses/{misuse_muid}', function() use ($app) {
-        $app->post('/tags/add',\MuBench\ReviewSite\Controller\TagController::class.":postTag")->setName('private.tag.add');
-        $app->post('/tags/{tag_id}/delete', \MuBench\ReviewSite\Controller\TagController::class.":deleteTag")->setName('private.tag.remove');
-
-        $app->post('/reviewers/{reviewer_id}', \MuBench\ReviewSite\Controller\ReviewController::class.":postReview")->setName('private.update.review');
-        // /snippets/{snippet_id}
-        $app->post('/snippets', \MuBench\ReviewSite\Controller\SnippetController::class.":postSnippet")->setName('private.snippet.add');
-        $app->post('/snippets/{snippet_id}/delete', \MuBench\ReviewSite\Controller\SnippetController::class.":deleteSnippet")->setName('private.snippet.remove');
+    $app->group('/experiments/{experiment_id}/detectors/{detector_muid}/projects/{project_muid}/versions/{version_muid}', function() use ($app) {
+        $app->post('/runs', \MuBench\ReviewSite\Controller\RunsController::class.":postRun");
+        $app->group('/misuses/{misuse_muid}', function() use ($app) {
+            $app->post('/tags', \MuBench\ReviewSite\Controller\TagController::class . ":postTag")->setName('private.tag.add');
+            $app->post('/tags/{tag_id}/delete', \MuBench\ReviewSite\Controller\TagController::class . ":deleteTag")->setName('private.tag.remove');
+            $app->post('/reviews/{reviewer_id}', \MuBench\ReviewSite\Controller\ReviewController::class . ":postReview")->setName('private.update.review');
+            $app->post('/snippets', \MuBench\ReviewSite\Controller\SnippetController::class . ":postSnippet")->setName('private.snippet.add');
+            $app->post('/snippets/{snippet_id}/delete', \MuBench\ReviewSite\Controller\SnippetController::class . ":deleteSnippet")->setName('private.snippet.remove');
+        });
     });
 
 })->add(new \MuBench\ReviewSite\Middleware\AuthMiddleware($container));
